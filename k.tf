3db8882ca77dcf1fe818318adc4b70d263fff720
Iamneo <techadmin@iamneo.ai>
2023-09-22 14:35:24 +0000

    Backup Commit

---
+++ k.tf
@@ -0,0 +1,13 @@
+terraform{
+    required_providers{
+        aws={
+            source="hashicorp/aws"
+        }
+    }
+}
+provider "aws"{
+    region=""
+    access_key=""
+    secret_key=""
+}
+resource "aws_instance""k"
