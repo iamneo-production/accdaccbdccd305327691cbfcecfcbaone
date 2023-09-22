98a0088940826ec6df072044311b76616462c9ee
Iamneo <techadmin@iamneo.ai>
2023-09-22 14:40:24 +0000

    Backup Commit

--- k.tf
+++ k.tf
@@ -6,8 +6,14 @@ terraform{
     }
 }
 provider "aws"{
-    region=""
-    access_key=""
-    secret_key=""
+    region="ap-northeast-1"
+    access_key="AKIAWO7URR7NR7ZVXS5Q"
+    secret_key="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"
 }
-resource "aws_instance""k"
+resource "aws_instance""kavi"{
+    ami="ami-0ed99df77a82560e6"
+    instance_type=""
+}
+output "public_ip"{
+    value=aws_instance.kavi.public_ip
+}
